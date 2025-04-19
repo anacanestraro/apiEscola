5ab9744fec958689297a4f07ff10db4e43e24616
anacanestraro <ana.canestraro07@gmail.com>
2025-04-18 21:02:16 -0300

    crud: Disciplina

--- src/controllers/DisciplinaController.ts
+++ src/controllers/DisciplinaController.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
+import { Op } from "sequelize";
 import { Disciplina } from "../models/Disciplina";
+import { AlunoDisciplina } from "../models/AlunoDisciplina";
 
-// 🔹 Listar disciplinas
 export const listarDisciplinas = async (req: Request, res: Response) => {
-    const disciplinas = await Disciplina.findAll();
-    return res.json(disciplinas);
+    try {
+        const disciplinas = await Disciplina.findAll();
+        return res.json(disciplinas);
+
+    }catch(error){
+        return res.status(500).json({error: "Erro ao buscar disciplinas."});
+    }
 }
 
-// 🔹 Cadastrar disciplina
 export const cadastrarDisciplina = async (req: Request, res: Response) => {
-    const { nome } = req.body;
+    const { nome, professorId } = req.body;
 
     if (nome) {
         let disciplinaExistente = await Disciplina.findOne({ where: { nome } });
         if (!disciplinaExistente) {
-            let novaDisciplina = await Disciplina.create({ nome });
+            let novaDisciplina = await Disciplina.create({ nome, professorId });
 
             res.status(201);
             return res.json({
@@ -26,26 +31,65 @@ export const cadastrarDisciplina = async (req: Request, res: Response) => {
         }
     }
 
-    return res.status(400).json({ error: "Nome da disciplina não enviado." });
+    return res.status(400).json({ error: "Erro no servidor." });
 };
-
 
 export const atualizarDisciplina = async (req: Request, res: Response) => {
 
     try {
-        const {disciplinaID} = req.params
+        const {disciplinaId} = req.params
         const dadosAtualizados = req.body
     
-        const disciplina = await Disciplina.findByPk(disciplinaID);
+        const disciplina = await Disciplina.findByPk(disciplinaId);
 
         if(!disciplina){
             return res.status(400).json({message: "Disciplina não encontrado"})
         }
-        await disciplina.update(dadosAtualizados, { fields: Object.keys(dadosAtualizados) });
-    
+        const disciplinaAtualizada = await disciplina.update(dadosAtualizados, { fields: Object.keys(dadosAtualizados) });
+        return res.status(200).json({message: "Disciplina atualizada com sucesso."});
+        
         
     } catch (error) {
         return res.status(400).json({message: "Erro do sistema"})
     }
    
+}
+
+export const deletarDisciplina = async (req: Request, res: Response) => {
+    try {
+        const {disciplinaId} = req.params;
+        const alunosVinculados = await AlunoDisciplina.count({
+            where: {disciplinaId}
+        });
+
+        if(alunosVinculados> 0) {
+            return res.status(400).json({error: "Disciplina não pode ser deletada pois possui alunos vinculados."});
+        }
+        const disciplina = await Disciplina.findByPk(disciplinaId);
+
+        if(!disciplina){
+            return res.status(404).json({error: "Disciplina não encontrada."});
+        }
+
+        await disciplina.destroy();
+        return res.status(200).json({message: "Disciplina deletada com sucesso"});
+    }catch(error){
+        return res.status(400).json({error: "Erro no servidor."});
+    }
+};
+
+export const buscarDisciplina = async (req: Request, res: Response) => {
+    try {
+        const {disciplinaId} = req.params;
+        const disciplina = await Disciplina.findByPk(disciplinaId);
+
+        if(!disciplina){
+            return res.status(404).json({error: "Disciplina não encontrada."});
+        }
+        
+        return res.json(disciplina);
+
+    }catch(error){
+        return res.status(400).json({error: "Erro no servidor."})
+    }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -18,6 +18,11 @@ router.get('/buscarAlunoId/:alunoId', AlunoController.buscarAluno)
 router.get('/listarTodasDisciplinas', DisciplinaController.listarDisciplinas);
 router.post('/cadastrarDisciplina', DisciplinaController.cadastrarDisciplina);
 router.put('/atualizarDisciplina/:disciplinaId', DisciplinaController.atualizarDisciplina);
+router.delete('/deletarDisciplina/:disciplinaId', DisciplinaController.deletarDisciplina);
+router.get('/buscarDisciplina/:disciplinaId', DisciplinaController.buscarDisciplina);
+
+
+
 router.post("/vincularAlunoADisciplina", AlunoDisciplinaController.vincularAlunoADisciplina);
 router.get("/listarDisciplinasDoAluno/:alunoId", AlunoDisciplinaController.listarDisciplinasDoAluno);
 
