83496b0dfc18ca5570551823379e248ff4712308
anacanestraro <ana.canestraro07@gmail.com>
2025-06-05 18:47:46 -0300

    teste: RotasAcademicas

---
+++ src/__tests__/RotasAcademicas.test.ts
@@ -0,0 +1,73 @@
+import server from "../server";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+import { response } from "express";
+
+
+const JWT_SECRET = process.env.JWT_SECRET || "senha-secreta";
+
+describe('Testes de funcionalidades ', () => {
+    const tokenProfessor = jwt.sign({tipo: "professor"}, JWT_SECRET);
+    const tokenAluno = jwt.sign({tipo: "aluno"}, JWT_SECRET);
+
+    it('NotasPorAluno - deve retornar notas de um aluno', async () =>{
+        const response = await request(server)
+        .get('/notasPorAluno/1')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(200);
+    });
+    it('NotasPorAluno - não deve permitir acesso de aluno', async () =>{
+        const response = await request(server)
+        .get('/notasPorAluno/1')
+        .set('Authorization', `Bearer ${tokenAluno}`);
+        expect(response.status).toBe(403);
+    }); 
+    it('NotasPorAluno - deve retornar erro aluno inexistente', async () =>{
+        const response = await request(server)
+        .get('/notasPorAluno/91299239')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(404);
+    }); 
+
+    it('PresencasPorAluno - deve retornar as presencas de uma aluno', async () =>{
+        const response = await request(server)
+        .get('/presencasPorAluno/1')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(200);
+    });
+    it('PresencasPorAluno - não deve retornar as presencas de uma aluno', async () =>{
+        const response = await request(server)
+        .get('/presencasPorAluno/1')
+        .set('Authorization', `Bearer ${tokenAluno}`);
+        expect(response.status).toBe(403);
+    });
+
+    it('PresencasPorAluno - deve retornar erro aluno inexistente', async () =>{
+        const response = await request(server)
+        .get('/presencasPorAluno/928393')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(404);
+    });
+
+    it('SituacaoPorAluno - deve retornar a situação de um aluno', async() => {
+        const response = await request(server)
+        .get('/situacaoPorAluno/1')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(200);
+    })
+
+    it('SituacaoPorAluno - não deve retornar a situação de um aluno', async() => {
+        const response = await request(server)
+        .get('/situacaoPorAluno/1')
+        .set('Authorization', `Bearer ${tokenAluno}`);
+        expect(response.status).toBe(403);
+    })
+
+    it('SituacaoPorAluno - deve retornar erro aluno inexistente', async() => {
+        const response = await request(server)
+        .get('/situacaoPorAluno/39898439')
+        .set('Authorization', `Bearer ${tokenProfessor}`);
+        expect(response.status).toBe(404);
+    })
+
+});
